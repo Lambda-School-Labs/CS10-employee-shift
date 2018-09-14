25959c0e892ea411da98a80cde745b2a4a31ff72
Obo Agboghidi <oagboghi2@gmail.com>
2018-09-14 14:00:26 -0700

    commit setings

--- front-end/src/components/Pages/Settings.js
+++ front-end/src/components/Pages/Settings.js
@@ -21,57 +21,43 @@ class Settings extends Component {
 
   render() {
     return (
+    <div>
       <Card className="card">
-        <form onSubmit={this.submitForm}>
-          <CardBody className="card_body">
-            <div>
-              <label>Email</label>
-              <input
-                value={this.state.email}
-                onChange={this.inputChangeHandler}
-                name="email"
-                type="text"
-              />
-            </div>
-            <div>
-              <label> Phone</label>
-              <input
-                value={this.state.phone}
-                onChange={this.inputChangeHandler}
-                name="email"
-                type="text"
-              />
-            </div>
+          <form onSubmit={this.submitForm}>
+            <CardBody className="card_body">
+              <div>
+                <label>Email</label>
+                <input className='textbox' value={this.state.email} onChange={this.inputChangeHandler} name="email" type="text" />
+              </div>
+              <div>
+                <label> Phone</label>
+                <input value={this.state.phone} onChange={this.inputChangeHandler} name="email" type="text" />
+              </div>
 
-            <label>email</label>
-            <input type="checkbox" name="msg" value="Email" />
-            <label>text</label>
-            <input type="checkbox" name="msg" value="text" />
+              <label>email</label>
+              <input type="checkbox" name="msg" value="Email" />
+              <label>text</label>
+              <input type="checkbox" name="msg" value="text" />
 
-            <div>
-              <label>Old Password</label>
-              <input
-                value={this.state.old_password}
-                onChange={this.inputChangeHandler}
-                name="old_password"
-                type="password"
-              />
-            </div>
-            <div>
-              <label>New Password</label>
-              <input
-                value={this.state.new_password}
-                onChange={this.inputChangeHandler}
-                name="new_password"
-                type="password"
-              />
-            </div>
-            <div>
-              <button type="submit">Save</button>
-            </div>
-          </CardBody>
-        </form>
-      </Card>
-    );
-  }
+              <div>
+                <label>Old Password</label>
+                <input value={this.state.old_password} onChange={this.inputChangeHandler} name="old_password" type="password" />
+              </div>
+              <div>
+                <label>New Password</label>
+                <input value={this.state.new_password} onChange={this.inputChangeHandler} name="new_password" type="password" />
+              </div>
+              <div>
+                <button type="submit">Save</button>
+              </div>
+            </CardBody>
+          </form>
+        </Card>
+      < Route exact path = "../navbar/sidebar.js" component = { Sidebar } />
+      </div>
+    )
+  };
 }
+
+
+export default Settings;
