a6b7e58ac56be173e8eb72d2df2c6963234e875b
Justin Kaseman <justinkaseman@live.com>
2018-09-17 13:33:39 -0700

    Add calendar organisms, molecules, and atoms

--- front-end/src/components/Pages/Calendar.js
+++ front-end/src/components/Pages/Calendar.js
@@ -1,9 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
+import CalendarTopNav from "../Organisms/CalendarTopNav.js";
+import Schedule from "../Organisms/Schedule.js";
 
-class Calendar extends Component {
-  render() {
-    return <div />;
-  }
-}
+const Calendar = () => {
+  return (
+    <div>
+      <CalendarTopNav />
+      <Schedule />
+    </div>
+  );
+};
 
 export default Calendar;
