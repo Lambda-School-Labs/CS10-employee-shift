edc2fd1a5dae1b1fccb993a75647f42b335d4c34
kyle <kauzlarich.kyle@gmail.com>
2018-09-19 08:52:01 -0700

    Styled component converted to styled components from css

---
+++ front-end/src/styles/Employee.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Top = styled.div`
+
+`;
+export const NavBar = styled.div`
+    display: flex;
+    padding: 1em;
+`;
+
+export const Bottom = styled.div`
+    display: flex;
+`;
+
+export const Left = styled.div`
+    padding-left: .5em;
+    padding-right: .5em;
+`;
+
+export const Right = styled.div`
+    display: flex;
+    padding-left: 1em;
+`;
+export const EmployeeCard = styled.div`
+    border: 1px solid black;
+    font-size: .5em;
+`;
+
+export const CardInner = styled.div`
+    border: 1px solid black;
+    max-width: 80%;
+    margin-left: 16px;
+    margin-bottom: 16px;
+`;
+
+export const NewEmployeeCard = styled.div`
+    border: 1px solid black;
+`;
