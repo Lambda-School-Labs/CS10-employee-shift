28ecf75e3fa3be20d9a4c9fdb75031b465bdebb8
Obo Agboghidi <oagboghi2@gmail.com>
2018-09-20 12:47:04 -0700

    worked on creating resuabl components for dashboards,and started redux for dashboard

---
+++ front-end/src/components/Organisms/TimeOffApproved.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TimeOffApproved = () => {
+    return (
+        <div> TimeOffApproved</div>
+    )
+}
+
+export default TimeOffApproved;
--- front-end/src/components/Pages/Dashboard.js
+++ front-end/src/components/Pages/Dashboard.js
@@ -1,34 +1,49 @@
 import React, { Component } from "react";
+import { connect } from "react-redux"; 
+import AssignedShift from "../Organisms/AssignedShift.js"; 
+import TimeOffApproved from "../Organisms/TimeOffApproved.js";
+import TimeOffRequest from "../Organisms/TimeOffRequests.js";
+
+import { DashboardContainer } from "../../styles/signin.js";
+import { HorizontalContainer } from "../../styles/Template--main.js";
+
+
+
+
+//import { postRequestOff } from '../../store/requestOff/action.js';
 
 class Dashboard extends Component {
   render() {
+    console.log(this.props)
     return (
-      /* TODO: Pull out into Molecular components */
-      /* TODO: Styled components */
-      <div className="emp-container">
+      <DashboardContainer>
         <h1>Welcome Employee</h1>
-        <div classname="emp-inner-container">
-          <div>
-            <h1>assigned</h1>
-          </div>
-
-          <div>
-            <h1>Time off approved</h1>
-          </div>
-
-          <div>
-            <h1>time off requested</h1>
-            <form action="">
-              Date: <input type="text" name="date" placeholder="date" /> <br />
-              Reason: <input type="text" name="reason" placeholder="reason" />
-              <br />
-              <input type="submit" value="submit" />
-            </form>
-          </div>
-        </div>
-      </div>
+        <HorizontalContainer>
+            <AssignedShift/>
+            <TimeOffApproved/>
+            <TimeOffRequest/>
+        </HorizontalContainer>
+      </DashboardContainer>
     );
   }
 }
 
-export default Dashboard;
+const mapStateToProps = state => {
+  return {
+    //firstname: state.user.currentUser.firstname,
+    //lastname: state.user.currentUser.lastname
+    //approvedTime
+    isAuthenticated: state.user.isAuthenticated
+  }
+}
+
+// const mapsDispatchtoProps = dispatch => {
+//   return {
+//     postRequestOff: (startTime, endTime) => {
+//       return dispatch(postRequestOff(startTime, endTime))
+//     }
+//   }
+// }
+
+export default connect(mapStateToProps, null)(Dashboard);
+//export default Dashboard;
