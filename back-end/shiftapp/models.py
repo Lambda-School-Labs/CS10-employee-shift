31b37a156911d4840a6f59490d3365fc96c66cab
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-09-20 15:34:12 -0400

    Database refactoring - calendar, day tables removed

--- back-end/shiftapp/models.py
+++ back-end/shiftapp/models.py
@@ -2,68 +2,96 @@ from django.db import models
 from uuid import uuid4
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 
-class User(AbstractUser):
-    telephone = models.CharField(max_length=14)
-    birth_date = models.DateField(null=True, blank=True)
-
-class Employer(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-    company_name = models.CharField(max_length=200)
+class Account(models.Model):
+    logo = models.URLField(max_length=200)
+    company = models.CharField(max_length=200)
+    enabled = models.BooleanField(default=True)
+    plan_expires = models.DateTimeField(auto_now=False, auto_now_add=False)
 
     def __str__(self):
-      return self.company_name
+        return self.company
+
 
 class HourOfOperation(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-
-    start_time = models.TimeField(auto_now=False, auto_now_add=False)
-    end_time = models.TimeField(auto_now=False, auto_now_add=False)
-
-class CalendarDay(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
-    hour_of_operation = models.ForeignKey(HourOfOperation, on_delete=models.CASCADE)
-
-    date = models.DateField(auto_now_add=False)
-    is_active = models.BooleanField(default=True)
-    is_holiday = models.BooleanField(default=False)
-    is_weekend = models.BooleanField(default=False)
+    DAY_CHOICES = (
+        ('M', 'Monday'),
+        ('T', 'Tuesday'),
+        ('W', 'Wednesday'),
+        ('R', 'Thursday'),
+        ('F', 'Friday'),
+        ('S', 'Saturday'),
+        ('U', 'Sunday')
+    )
     
-class Employee(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    day = models.CharField(
+        max_length=1,
+        choices=DAY_CHOICES,
+        default='M'
+    )
+    open_time = models.TimeField(auto_now=False, auto_now_add=False)
+    close_time = models.TimeField(auto_now=False, auto_now_add=False)
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    phone_number = models.CharField(max_length=14)
+    notes = models.TextField(max_length=400)
+
 
 class RequestedTimeOff(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    STATUS_CHOICES = (
+        ('P', 'Pending'),
+        ('C', 'Canceled'), 
+        ('A', 'Accepted'), 
+        ('E', 'Expired')
+    )
 
-    date = models.DateField(auto_now_add=False)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    start_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+    end_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     reason = models.CharField(max_length=200)
-    is_approved = models.BooleanField(default=False)
-    
-class Day(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    day_name = models.CharField(max_length=15)
-    abbreviation_name = models.CharField(max_length=1)
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        default='P'
+    )
 
-    def __str__(self):
-      return self.day_name 
-    
-    
+       
 class Shift(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    calendarday = models.ForeignKey(CalendarDay, on_delete=models.CASCADE)
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    day = models.ForeignKey(Day, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True)
 
+    start_datetime =  models.DateTimeField(auto_now=False, auto_now_add=False)
+    end_datetime =  models.DateTimeField(auto_now=False, auto_now_add=False)
+    notes = models.TextField(max_length=400)
+    is_open = models.BooleanField(default=True)
+
+
+class Availability(models.Model):
+    DAY_CHOICES = (
+        ('M', 'Monday'),
+        ('T', 'Tuesday'),
+        ('W', 'Wednesday'),
+        ('R', 'Thursday'),
+        ('F', 'Friday'),
+        ('S', 'Saturday'),
+        ('U', 'Sunday')
+    )
+    
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    day = models.CharField(
+        max_length=1,
+        choices=DAY_CHOICES,
+        default='M'
+    )
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
     end_time = models.TimeField(auto_now=False, auto_now_add=False)
-    status = models.BooleanField(default=True)
-    is_available = models.BooleanField(default=True)
