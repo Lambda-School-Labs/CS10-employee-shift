ca9f646030c98629f7cfabaa62fe8031313a42e9
TERR1E <terrieb.kim@gmail.com>
2018-09-27 13:50:19 -0700

    icons added

--- front-end/src/components/Organisms/landing_detail.js
+++ front-end/src/components/Organisms/landing_detail.js
@@ -7,6 +7,10 @@ import {
 
 import "../../styles/LandingRef.css";
 
+import Accountability from "../../styles/icons/benefits-accountability.png"
+import Changes from "../../styles/icons/benefits-changes.png"
+import Time from "../../styles/icons/benefits-time.png"
+
 const divStyle = {
     // backgroundColor: '#b8b8b8'
     backgroundColor: '#e6e6e6'
@@ -23,16 +27,19 @@ const Landing_Details = () => {
 
           <div className="mid-container">
             <div className="textBox">
+              <img className="icon" src={Time} alt="time" />
               <h2 className="vertical title-text">Spend More Time Growing Your Business</h2>
               <h6 className="vertical inner-text">Spend more time working to grow your business and less time fighting fires.</h6>
             </div>
 
             <div className="textBox">
+              <img className="icon" src={Accountability} alt="accountability" />
               <h2 className="vertical title-text">Increase Employee Accountability</h2>
               <h6 className="vertical inner-text">Reduce employee no-shows and improve accountability across your team by 25%</h6>
             </div>
 
             <div className="textBox">
+              <img className="icon" src={Changes} alt="changes" />
               <h2 className="vertical title-text">Handle Changes with Ease</h2>
               <h6 className="vertical inner-text">Stop rebuilding schedules by reviewing and approving employee requests in real-time.</h6>
             </div>
