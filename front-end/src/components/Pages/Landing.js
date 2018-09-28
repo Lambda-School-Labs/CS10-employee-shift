6f5d642607da2f83858d4fd8fe94d346253abd36
TERR1E <terrieb.kim@gmail.com>
2018-09-28 12:30:29 -0700

    Landing page styling in progress

--- front-end/src/components/Pages/Landing.js
+++ front-end/src/components/Pages/Landing.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import Landing_Card from "../Organisms/landing_card.js"
 import Landing_Detail from "../Organisms/landing_detail.js"
+import Landing_Learning from '../Organisms/landingLearn'
 
 const Landing = () => {
   return (
     <div>
-      <Landing_Card/>
-      <Landing_Detail/>
-      <LandingLearning/>
+      <Landing_Card />
+      <Landing_Detail />
+      <Landing_Learning />
       {/*<LandingFooter/>*/}
     </div>
   )
