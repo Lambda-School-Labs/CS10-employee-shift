92668fc2372d30fd525be6a7527a1d76648d4ba6
Justin Kaseman <justinkaseman@live.com>
2018-10-11 09:04:16 -0700

    Employee CRUD finished

--- front-end/src/store/Profile/reducer.js
+++ front-end/src/store/Profile/reducer.js
@@ -7,7 +7,7 @@ const initialState = {
 
 export default (state = initialState, action) => {
   // return shallow copy of allProfiles
-  // const newProfiles = state.allProfiles.slice();
+  const newProfiles = state.allProfiles.slice();
 
   switch (action.type) {
     case "LOADING_PROFILE":
@@ -19,28 +19,23 @@ export default (state = initialState, action) => {
     case "READ_PROFILES":
       return { ...state, allProfiles: action.data, profileLoading: false };
 
-    /* For managers only?? REFACTOR FOLLOWING
-    */
-    // case "CREATE_PROFILE":
-    //   // DEV CONSOLE LOG, REMOVE ME!
-    //   console.log("CREATED SHIFT");
-    //   shifts.push(action.data);
-    //   return { ...state, currentProfile: shifts, profileLoading: false };
+    case "CREATE_PROFILE":
+      newProfiles.push(action.data);
+      return { ...state, allProfiles: newProfiles, profileLoading: false };
 
-    // case "UPDATE_PROFILE":
-    //   // DEV CONSOLE LOG, REMOVE ME!
-    //   console.log("UPDATED SHIFT");
-    //   // grab shift using its index, maybe handle this differently
-    //   const shiftToUpdate = shifts[action.index];
-    //   // do some updates
-    //   shifts.splice(action.index, 1, shiftToUpdate);
-    //   return { ...state, currentProfile: shifts, profileLoading: false };
+    case "UPDATE_PROFILE":
+      const updatedProfile = action.data;
+      const indexToUpdate = newProfiles.indexOf(
+        newProfiles.filter(profile => profile.id === updatedProfile.id)[0]
+      );
+      newProfiles.splice(indexToUpdate, 1, updatedProfile);
+      return { ...state, currentProfile: newProfiles, profileLoading: false };
 
-    // case "DELETE_PROFILE":
-    //   // DEV CONSOLE LOG, REMOVE ME!
-    //   console.log("DELETED SHIFT");
-    //   shifts.splice(action.index, 1);
-    //   return { ...state, currentProfile: shifts, profileLoading: false };
+    case "DELETE_PROFILE":
+      const new_profiles = newProfiles.filter(
+        profile => profile.id !== action.data
+      );
+      return { ...state, currentProfile: new_profiles, profileLoading: false };
 
     // TODO: double check this
     case "ERROR":
